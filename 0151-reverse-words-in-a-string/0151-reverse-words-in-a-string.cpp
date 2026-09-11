a746abd271975d3ef56c5eeb5c35532a459606a6
Jay Rathore <mailjayrathore@gmail.com>
2026-09-11 15:04:10 +0530

    Time: 0 ms (100%), Space: 11 MB (35.94%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string word;
+        vector<string> words;
+
+        // Words ko vector mein store karo
+        while (ss >> word) {
+            words.push_back(word);
+        }
+
+        // Reverse order mein sentence banao
+        string ans;
+
+        for (int i = words.size() - 1; i >= 0; i--) {
+            ans += words[i];
+
+           if(i!=0){
+            ans+=" ";
+           }
+        }
+
+        return ans;
+    }
+};
